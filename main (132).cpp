c9623a31458bc6b6cb8bb70dd78dba920d56a1b0
AnarbekAyazhan <58087467+AnarbekAyazhan@users.noreply.github.com>
2019-11-23 02:57:38 +0600

    Update main (132).cpp

--- main (132).cpp
+++ main (132).cpp
@@ -1,21 +1,11 @@
-#include <iostream>
+  
+#include "bits/stdc++.h"
 using namespace std;
-int main () {
-    int n;
-    int k;
-    int sum = 0;
-    bool b = true;
-    cin >> n >> k;
-    for (int i = 1; i <=n ; i++) {
-        sum += i;
-        if (sum == k) {
-            b = false;
-            break;
-        }
-    }
-    if (b == true) cout << "Not exist";
-    else cout << "Exist";
-    return 0;
+int main(){
+char c='*';
+cout<<c<<" "<<c<<" "<<c<<endl;
+cout<<' '<< c <<" "<< c <<' '<< c <<" "<< c <<endl;
+cout<<" "<<c<<' '<< c <<" "<< c <<' ' <<c<<endl;
+cout<<" "<<c<<" "<<c<<endl;
 }
-
 
